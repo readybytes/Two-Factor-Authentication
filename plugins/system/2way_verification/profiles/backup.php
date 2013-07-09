576cb992859515b96eab6dc8871fd35770307fca
Manish Trivedi <manish.tech2@gmail.com>
2013-07-09 13:45:18 +0530

    Update backup.php
    just add author

--- plugins/system/2way_verification/profiles/backup.php
+++ plugins/system/2way_verification/profiles/backup.php
@@ -2,6 +2,7 @@
 /**
  * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      mManishTrivedi
  */
 
 defined('JPATH_BASE') or die;
