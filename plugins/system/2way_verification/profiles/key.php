7e2a48df6e0e9e3a55ef10ee2c1dff85615de43e
Manish Trivedi <manish.tech2@gmail.com>
2013-07-09 13:49:43 +0530

    Update key.php
    Add author

--- plugins/system/2way_verification/profiles/key.php
+++ plugins/system/2way_verification/profiles/key.php
@@ -1,7 +1,8 @@
 <?php
 /**
  * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @license	GNU General Public License version 2 or later; see LICENSE.txt
+ * @author	mManishTrivedi
  */
 
 defined('JPATH_BASE') or die;
